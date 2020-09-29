93a52f381103e9c0e759010d885b6fee9d561216
Kwame Forbes <kwameforbes1000@gmail.com>
2020-09-29 18:31:55 -0400

    Building Function

--- R/vizWithSCE.R
+++ R/vizWithSCE.R
@@ -1,6 +1,10 @@
+install.packages("devtools")
+library(devtools)
+devtools::install_github
 install_github("mikelove/DESeq2")
 library(DESeq2)
-package Version("DESeq2)
+packageVersion("DESeq2")
+
 
 plotter <- function(dat) {
   stopifnot(all(names(dat) == c("res", "dds", "ans")))
