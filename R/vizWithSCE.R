d063f7dffe581496941828335632633322617089
Kwame Forbes <kwameforbes1000@gmail.com>
2020-10-05 15:51:59 -0400

    Working on documentation

--- R/vizWithSCE.R
+++ R/vizWithSCE.R
@@ -172,7 +172,27 @@ source("R/integrate.R")
 
 dat <- integrateWithSingleCell(res,dds)
 
-plotter <- function(dat) {
+#' Visualize the integration of bulk DE results with Bioconductor single-cell RNA-seq datasets
+#'
+#' @description A package that assists with the visualization of the integration of bulk DE results tables with pre-processed scRNA-seq datasets available on Bioconductor, for downstream visualization tasks. After the user has pick a scRNA-seq dataset from integrateWithSingleCell.... The output of the function is...
+#'
+#'
+#' @param dat this is a variable of integratewithSingleCell
+#'
+#' @return nothing
+#'
+#' @examples
+#'
+#' \dontrun{
+#' dat <- integrateWithSingleCell(res,dds)
+#' vizWithSCE(dat)
+#' }
+#'
+#' @author Michael Love
+#' @author Kwame Forbes
+#'
+#' @export
+vizWithSCE <- function(dat) {
   stopifnot(all(names(dat) == c("res", "dds", "sce")))
   plot(dat$res, dat$dds)
 }
